2e24a5da4f661bd697b5c69418c23bcc0e6f111e
LuRnEsTi <luukas.vuonokari@gmail.com>
2024-01-11 22:55:08 +0200

    11.1.2024. Korttien lisäys ja poistaminen manuaalisesti.

--- logiikka.py
+++ logiikka.py
@@ -13,7 +13,32 @@ def deal_hands(pakka):
     kasi1 = pakka[:5]
     kasi2 = pakka[5:10]
     loput_kortit=pakka[10:]
-    eka_kortti=pakka[:5]
-    return kasi1, kasi2, loput_kortit, eka_kortti
+    #eka_kortti=pakka[:5]
+    return kasi1, kasi2, loput_kortit#, eka_kortti
+
+def pakan_vahennys(pakka,poistettavat_kortit):
+    poistetut_kortit=[]
+    for card in poistettavat_kortit:
+        if card in pakka:
+            pakka.remove(card)
+            poistetut_kortit.append(card)
+        print(pakka)
+
+        if poistetut_kortit:
+            print(f"poistettiin{', '.join(poistetut_kortit)}kadesta")
+
+def pakan_lisays(hand, remaining_cards):
+    while len(hand) < 5 and remaining_cards:
+        new_card = remaining_cards.pop(0)
+        hand.append(new_card)
+        print(f"Added {new_card} to the hand.")
+
+
+
+    #else:
+        #print("no ei löytyny nnjiitä korttjea")
+    #return poistettavat_kortit
+#def pakan_paivitys(deal_hands):
+    
 
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
-from paskahousu.sprite_sheet import load_sprite_sheet, maarita_alueet, display_kortit,display_kortti
+from sprite_sheet import load_sprite_sheet, maarita_alueet, display_kortit,display_kortti
 #from kortti import korttien_maara, leveys, korkeus, vali, kokonaisleveys, paikka
 from kortti import kortti
-from logiikka import deal_hands, create_pakka, shuffle_pakka
+from logiikka import deal_hands, create_pakka, shuffle_pakka, pakan_vahennys,pakan_lisays
 
 
 ##pygame setuppi
@@ -24,12 +24,12 @@ taustakuva_rect = taustakuva.get_rect()
 unshuffled_pakka = create_pakka()
 shuffle_pakka(unshuffled_pakka)
 
-kasi1, kasi2, loput_kortit, kasi1 = deal_hands(unshuffled_pakka)
+kasi1, loput_kortit,_ = deal_hands(unshuffled_pakka)
 
 #print("Pelaaja 1:", kasi1)
-#print("Pelaaja 2:", kasi2)
+#print("Pelaaja 2:", kasi2)g/1
 #print("Loppu pakka: ",loput_kortit)
-#print("Pakassa olevien korttien määrä",len(loput_kortit))
+print("Pakassa olevien korttien määrä",len(loput_kortit))
 print("ensimmäinen kortti on: ",kasi1)
 
 
@@ -37,7 +37,7 @@ kortti_alueet = maarita_alueet()
 card_ids_to_display = kasi1
 
 
-
+poistettavat_kortit = kasi1[:1]
 running = True
 while running:
   
@@ -49,14 +49,23 @@ while running:
     screen.blit(taustakuva, taustakuva_rect) 
     cursor_position = pygame.mouse.get_pos()
     pressed_buttons = pygame.mouse.get_pressed()
+
+    card_ids_to_display = kasi1[:5]
+    
+    pakan_vahennys(kasi1,poistettavat_kortit)
+    pakan_lisays(kasi1,loput_kortit)
+
+
     display_kortit(card_ids_to_display, kortti_alueet, sprite_sheet_kuva, screen, cursor_position, pressed_buttons)
     #display_kortti(card_ids_to_display, kortti_alueet, sprite_sheet_kuva, screen, (5,5))
     
+
+
 
     
 
     pygame.display.update()
 
  
-#print(unshuffled_pakka)
+print(unshuffled_pakka)
 pygame.quit()
